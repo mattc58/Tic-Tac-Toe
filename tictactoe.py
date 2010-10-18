b1189f8503ad45ed05a220abe8b787eb855c9f5d
Matt Culbreth <mattc@hemispheres.local>
2010-10-18 09:39:40 -0400

    first few pieces of game mechanics

--- tictactoe.py
+++ tictactoe.py
@@ -20,21 +20,72 @@ board = [
     [None, None, None]
 ]
 
+columns = {
+    'A' : 0,
+    'B' : 1,
+    'C' : 2
+}
+
 def print_board():
     '''
     Print the board to the console
     '''
-    for row in board:
-        print " | ".join([item or ' ' for item in row])
+    print "    A | B | C"
+    print "-" * 14
+    for i in range(len(board)):
+        s = "%d|  " % (i + 1)
+        row = board[i]
+        s += " | ".join([item or ' ' for item in row])
+        print s
+        
+def calc_computer_move():
+    '''
+    This is a function to calculate the computer's move
+    '''
+    return 'A1'
 
 def main():
+    '''
+    The main function with the main game loop
+    '''
     print "***********************************************"
     print "Hello, this is Matt Culbreth's Tic-Tac-Toe game."
     print "Good luck! (You'll need it.)"
     print "***********************************************"
     
+    # let's choose a piece at random
     user_piece = random.choice(game_pieces)
     print "You are playing %s\n" % user_piece
+    
+    # the main game loop.
+    # algorithm:
+    #   show the board
+    #   let X go
+    #   if winner, end
+    #   show the board
+    #   let O go
+    #   if winner, end
+    #   show the board
+    next_move = user_piece if user_piece == 'X' else 'O'
+    while True:
+        print_board()
+        
+        # get the move from the active user (user or computer)
+        if next_move == user_piece:
+            move = raw_input("Please choose your move by typing coordinates (like A1 or B2):")
+        else:
+            move = calc_computer_move()
+            
+        # validate the move to be A1 through C3
+            
+        # update the game board
+        column = columns[move[0]]
+        row = int(move[1]) - 1
+        board[row][column] = next_move
+        
+        next_move = 'X' if next_move == 'O' else 'O'    
+        
+        
 
 
 if __name__ == '__main__':
