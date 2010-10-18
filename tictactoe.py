1f7e74247e5dddfc054f35b36c3612fb381e4f3d
Matt Culbreth <mattc@hemispheres.local>
2010-10-18 14:26:32 -0400

    moved game mechanics and board state into a class

--- tictactoe.py
+++ tictactoe.py
@@ -14,7 +14,6 @@ import os
 import random
 import copy
 
-board = None
 columns = {
     'A' : 0,
     'B' : 1,
@@ -22,138 +21,152 @@ columns = {
 }
 valid_moves = ('A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3')
 
+class TicTacToe(object):
+    '''
+    The class for the game
+    '''
+    def __init__(self):
+        '''
+        Init the game board
+        '''
+        self.board = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None]
+        ]
 
-def print_board():
-    '''
-    Print the board to the console
-    '''
-    print "    A | B | C"
-    print "-" * 14
-    for i in range(len(board)):
-        s = "%d   " % (i + 1)
-        row = board[i]
-        s += " | ".join([item or ' ' for item in row])
-        print s
-    print "-" * 14
+    def print_board(self):
+        '''
+        Print the self.board to the console
+        '''
+        print "    A | B | C"
+        print "-" * 14
+        for i in range(len(self.board)):
+            s = "%d   " % (i + 1)
+            row = self.board[i]
+            s += " | ".join([item or ' ' for item in row])
+            print s
+        print "-" * 14
     
-def is_board_full():
-    '''
-    Return True if the board is full
-    '''
-    empty = [None in row for row in board]
-    return empty == [False, False, False]
+    def is_board_full(self):
+        '''
+        Return True if the self.board is full
+        '''
+        empty = [None in row for row in self.board]
+        return empty == [False, False, False]
 
-def check_cells(cells):
-    '''
-    Check a passed in set of cells for a winning solution
-    '''    
-    return (None not in cells) and (cells[0] == cells[1] == cells[2])
+    def _check_cells(self, cells):
+        '''
+        Check a passed in set of cells for a winning solution
+        '''    
+        return (None not in cells) and (cells[0] == cells[1] == cells[2])
     
-def is_winner(test_board):
-    '''
-    Return True if we have a winner
-    '''
-    # check for a row of all the same
-    cells = []
+    def is_winner(self, test_board=None):
+        '''
+        Return True if we have a winner
+        '''
+        if not test_board:
+            test_board = self.board
+            
+        # check for a row of all the same
+        cells = []
     
-    for row in test_board:
-        cells = [row[0], row[1], row[2]]
-        if check_cells(cells):
-            return cells[0]
+        for row in test_board:
+            cells = [row[0], row[1], row[2]]
+            if self._check_cells(cells):
+                return cells[0]
     
-    # for a column of all the same
-    for column in range(3):
-        cells = [test_board[0][column], test_board[1][column], test_board[2][column]]
-        if check_cells(cells):
-            return cells[0]
+        # for a column of all the same
+        for column in range(3):
+            cells = [test_board[0][column], test_board[1][column], test_board[2][column]]
+            if self._check_cells(cells):
+                return cells[0]
     
-    # check the two diagonals
-    diag1 = [test_board[0][0], test_board[1][1], test_board[2][2]]
-    diag2 = [test_board[0][2], test_board[1][1], test_board[2][0]]
-    if check_cells(diag1):
-        return diag1[0]
-    if check_cells(diag2):
-        return diag2[0]
+        # check the two diagonals
+        diag1 = [test_board[0][0], test_board[1][1], test_board[2][2]]
+        diag2 = [test_board[0][2], test_board[1][1], test_board[2][0]]
+        if self._check_cells(diag1):
+            return diag1[0]
+        if self._check_cells(diag2):
+            return diag2[0]
         
-    # if we come down here, no winner
-    return None
+        # if we come down here, no winner
+        return None
         
-def calc_computer_move(computer_piece, user_piece):
-    '''
-    This is a function to calculate the computer's move
-    '''
-    # make a local copy
-    test_board = copy.deepcopy(board)
+    def calc_computer_move(self, computer_piece, user_piece):
+        '''
+        This is a function to calculate the computer's move
+        '''
+        # make a local copy
+        test_board = copy.deepcopy(self.board)
     
-    # the plan: look for a winning solution, and take it. If not, block his.
-    for i in range(len(test_board)):
-        row = test_board[i]
-        for j in range(len(row)):
-            cell = row[j]
-            if not cell:
-                # empty square, so try it
-                test_board[i][j] = computer_piece
-                if is_winner(test_board):
-                    column = 'A' if j == 0 else 'B' if j == 1 else 'C'
-                    return '%s%d' % (column, i + 1)
-                else:
-                    # reset the board
-                    test_board[i][j] = None
+        # the plan: look for a winning solution, and take it. If not, block his.
+        for i in range(len(test_board)):
+            row = test_board[i]
+            for j in range(len(row)):
+                cell = row[j]
+                if not cell:
+                    # empty square, so try it
+                    test_board[i][j] = computer_piece
+                    if self.is_winner(test_board):
+                        column = 'A' if j == 0 else 'B' if j == 1 else 'C'
+                        return '%s%d' % (column, i + 1)
+                    else:
+                        # reset the self.board
+                        test_board[i][j] = None
                     
-    # now block the user's game winning moves if necessary
-    for i in range(len(test_board)):
-        row = test_board[i]
-        for j in range(len(row)):
-            cell = row[j]
-            if not cell:
-                # empty square, so try it
-                test_board[i][j] = user_piece
-                if is_winner(test_board):
-                    column = 'A' if j == 0 else 'B' if j == 1 else 'C'
-                    return '%s%d' % (column, i + 1)
-                else:
-                    # reset board
-                    test_board = copy.deepcopy(board)
+        # now block the user's game winning moves if necessary
+        for i in range(len(test_board)):
+            row = test_board[i]
+            for j in range(len(row)):
+                cell = row[j]
+                if not cell:
+                    # empty square, so try it
+                    test_board[i][j] = user_piece
+                    if self.is_winner(test_board):
+                        column = 'A' if j == 0 else 'B' if j == 1 else 'C'
+                        return '%s%d' % (column, i + 1)
+                    else:
+                        # reset self.board
+                        test_board = copy.deepcopy(self.board)
 
     
     
-    # stop cornering
-    if board[0][1] == user_piece and board[1][0] == user_piece and not board[0][0]:
-        return 'A1'
-    if board[0][1] == user_piece and board[1][2] == user_piece and not board[0][2]:
-        return 'C1'
-    if board[1][2] == user_piece and board[2][1] == user_piece and not board[2][2]:
-        return 'C3'
-    if board[1][0] == user_piece and board[2][1] == user_piece and not board[2][0]:
-        return 'A3'
+        # stop cornering
+        if self.board[0][1] == user_piece and self.board[1][0] == user_piece and not self.board[0][0]:
+            return 'A1'
+        if self.board[0][1] == user_piece and self.board[1][2] == user_piece and not self.board[0][2]:
+            return 'C1'
+        if self.board[1][2] == user_piece and self.board[2][1] == user_piece and not self.board[2][2]:
+            return 'C3'
+        if self.board[1][0] == user_piece and self.board[2][1] == user_piece and not self.board[2][0]:
+            return 'A3'
         
-    # triangles (B1, C2, B3, A2)
-    if board[1][1] == user_piece:
-        if not board[0][1] and (board[0][0] == user_piece or board[0][2] == user_piece):
-            return 'B1'
-        if not board[1][2] and (board[0][2] == user_piece or board[2][2] == user_piece):
-            return 'C2'
-        if not board[2][1] and (board[2][0] == user_piece or board[2][2] == user_piece):
-            return 'B3'
-        if not board[1][0] and (board[0][1] == user_piece or board[0][0] == user_piece):
-            return 'A2'
+        # triangles (B1, C2, B3, A2)
+        if self.board[1][1] == user_piece:
+            if not self.board[0][1] and (self.board[0][0] == user_piece or self.board[0][2] == user_piece):
+                return 'B1'
+            if not self.board[1][2] and (self.board[0][2] == user_piece or self.board[2][2] == user_piece):
+                return 'C2'
+            if not self.board[2][1] and (self.board[2][0] == user_piece or self.board[2][2] == user_piece):
+                return 'B3'
+            if not self.board[1][0] and (self.board[0][1] == user_piece or self.board[0][0] == user_piece):
+                return 'A2'
     
-    # no winning or game saving moves, so pick a cell out of a list of possibles, ordered
-    # by goodness
-    good_cells = ('B2', 'A1', 'C1', 'A3', 'C3', 'B1', 'A2', 'B3', 'C2')
-    for cell in good_cells:
-        column = columns[cell[0]]
-        row = int(cell[1]) - 1
-        if not board[row][column]:
-            return cell
+        # no winning or game saving moves, so pick a cell out of a list of possibles, ordered
+        # by goodness
+        good_cells = ('B2', 'A1', 'C1', 'A3', 'C3', 'B1', 'A2', 'B3', 'C2')
+        for cell in good_cells:
+            column = columns[cell[0]]
+            row = int(cell[1]) - 1
+            if not self.board[row][column]:
+                return cell
     
 
 def main():
     '''
     The main function with the main game loop
     '''
-    global board
-    
     print "***********************************************"
     print "Hello, this is Matt Culbreth's Tic-Tac-Toe game."
     print "Good luck! (You'll need it.)"
@@ -162,21 +175,18 @@ def main():
     # go in an infinite loop. kill the program to end the game.
     while True:
         # setup the board and the initial pieces
-        board = [
-               [None, None, None],
-               [None, None, None],
-               [None, None, None]
-           ]
+        game = TicTacToe()
+        
         user_piece = random.choice(('X', 'O'))
         computer_piece = 'X' if user_piece == 'O' else 'O'
         print "You are playing %s\n" % user_piece
     
         if user_piece == 'X':
-            print_board()
+            game.print_board()
     
         # the main game loop. Keep going until we have a winner or a full board.
         current_move = 'X'
-        while not is_winner(board) and not is_board_full():
+        while not game.is_winner() and not game.is_board_full():
             is_user_move = user_piece == current_move
         
             # get the move from the active user (user or computer)
@@ -184,7 +194,7 @@ def main():
                 move = raw_input("Please choose your move by typing coordinates (like A1 or B2):")
                 move = move.upper()
             else:
-                move = calc_computer_move(computer_piece, user_piece)
+                move = game.calc_computer_move(computer_piece, user_piece)
             
             # validate the move to be A1 through C3
             if is_user_move and move not in valid_moves:
@@ -196,30 +206,30 @@ def main():
             row = int(move[1]) - 1
         
             # make sure there's not an existing piece there
-            if is_user_move and board[row][column]:
+            if is_user_move and game.board[row][column]:
                 print "Please choose an empty cell."
                 continue
             
             # update the board
-            board[row][column] = current_move
+            game.board[row][column] = current_move
         
             # print the board if it's the computer's turn
             if current_move != user_piece:
-                print_board()
+                game.print_board()
             
             # change pieces
             current_move = 'X' if current_move == 'O' else 'O'    
     
         # print the final board
-        print_board()
+        game.print_board()
     
-        winner = is_winner(board)  
+        winner = game.is_winner()  
         if winner:
             if winner == user_piece:
                 print "You won! NOOOOOOOOOOOOOOOOO!"
             else:
                 print "HAH! Beat you."
-        elif is_board_full():
+        elif game.is_board_full():
             print "DRAW"
         else:
             print "Something I didn't consider possible"
